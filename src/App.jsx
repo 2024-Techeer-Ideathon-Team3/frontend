19aff9ea369af1e55f28913c585b4f1525599f9f
choihooo <hochoi8621@naver.com>
2024-06-01 01:15:30 +0900

    init

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+import './App.css'
+import { BrowserRouter } from 'react-router-dom'
+
+function App() {
+  return <></>
+}
+
+export default App
---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+// Home.js
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      <h2>홈 페이지</h2>
+      <p>이곳은 홈입니다.</p>
+    </div>
+  )
+}
+
+export default Home
