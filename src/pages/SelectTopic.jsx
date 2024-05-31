ca4d80d593cb978056699eef2fdabe297bc7e9d5
Yeongjun Joo <wndudwns6824@naver.com>
2024-06-01 05:38:31 +0900

    feat: 수정

--- src/pages/SelectTopic.jsx
+++ src/pages/SelectTopic.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+// import React from 'react'
 import Topic from '../components/Topic'
+import topic from '../assets/topic.png'
+import NavBar from '../components/NavBar'
 
 function SelectTopic() {
   return (
     <>
+      <NavBar />
       <div className="fixed">
         <div className="absolute inset-0 flex items-center justify-center mt-[10rem]">
           <Topic></Topic>
@@ -15,7 +18,7 @@ function SelectTopic() {
         <div className="flex items-center justify-center">
           <div className="border-dashed border-b border-gray-400 w-[20vw] h-[33.3vh]"></div>
           <div className=" flex items-center justify-center border-dashed border-b border-l border-r border-gray-400 w-[60vw] h-[33.3vh]">
-            <div className="text-[5em] pb-[1rem]">please select your topic</div>
+            <img src={topic} alt="topic" className="w-[680px]" />
           </div>
           <div className="border-dashed border-b border-gray-400 w-[20vw] h-[33.3vh]"></div>
         </div>
@@ -39,14 +42,3 @@ function SelectTopic() {
 }
 
 export default SelectTopic
-
-// border-gray-400
-{
-  /* <div className="flex items-center">
-<div className="border-dashed border-b border-gray-400 w-[20%] h-[33.3vh]"></div>
-<div className="border-dashed border-b border-l border-r border-gray-400 w-[20%] h-[33.3vh]"></div>
-<div className="border-dashed border-b border-gray-400 w-[20%] h-[33.3vh]"></div>
-<div className="border-dashed border-b border-l border-r border-gray-400 w-[20%] h-[33.3vh]"></div>
-<div className="border-dashed border-b border-gray-400 w-[20%] h-[33.3vh]"></div>
-</div> */
-}
--- src/utils/router.jsx
+++ src/utils/router.jsx
@@ -1,17 +1,15 @@
 import { createBrowserRouter } from 'react-router-dom'
-import MainPage from '../page/MainPage'
-
+import MainPage from '../pages/MainPage'
 import SelectTopic from '../pages/SelectTopic'
 const routers = createBrowserRouter([
-   {
+  {
     path: '/',
     element: <MainPage />,
   },
   {
-    path: '/SelectTopic',
+    path: '/topic',
     element: <SelectTopic />,
   },
- 
 ])
 
 export default routers
