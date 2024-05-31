8c2918810cf3272bf94cbfe233ecd8c1be7f9ab6
Yeongjun Joo <wndudwns6824@naver.com>
2024-06-01 05:30:38 +0900

    feat: 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
 import { RouterProvider } from 'react-router-dom'
-<<<<<<< feat/#1
-import routers from './utils/router'
-=======
-
 import routers from './utils/router'
 
->>>>>>> main
 export default function App() {
   return (
     <div className="h-full">
